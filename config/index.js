7a1df17a13cac1ea85167491632a2f6637a7cb7a
Richard Lindsay <richard@tictocfamily.com>
2018-02-12 11:18:24 +0000

    Another attempted fix

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/scotrail-excuse-generator',
 
     /**
      * Source Maps
