0137e1cb56b92c7c8bc77a949eaa5f4461d4cb79
Richard Lindsay <richard@tictocfamily.com>
2018-02-12 11:01:28 +0000

    another attempted fix for gh pages

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'scotrail-excuse-generator/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: 'scotrail-excuse-generator',
 
     /**
      * Source Maps
